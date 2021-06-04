0500bb01f58cc4c2e01745f59dc1349e8a405538
Dante Rodrigues <blog.anutrof@gmail.com>
2021-06-04 15:44:34 -0300

    sadsad

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,16 +1,37 @@
-
 //Gerente é um FuncionarioAutenticavel
-public class Gerente extends FuncionarioAutenticavel {
+public class Gerente extends Funcionario implements Autenticavel {
 		
 //	public Gerente(String nome, String cpf, double salario) {
 //		super(nome, cpf, salario);
 //		// TODO Auto-generated constructor stub
 //	}
 	
+	private int senha;
+	
+	
 	public double getBonificacao() {
 		System.out.println("Chamando o metodo de bonificacao do Gerente ");
 		return super.getSalario();
 	}
+
+
+	
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+		
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+
 	
 	
 }
--- bytebank-herdado/src/TesteSistema.java
+++ bytebank-herdado/src/TesteSistema.java
@@ -4,13 +4,13 @@ public class TesteSistema {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Gerente g = new Gerente();
-		g.setSenha(2322);
+		Autenticavel g = new Gerente();
+		g.setSenha(2222);
 		
 		SistemaInterno si = new SistemaInterno();
 		si.autentica(g);
 		
-		FuncionarioAutenticavel fa = new Administrador();
+		Autenticavel fa = new Administrador();
 		fa.setSenha(2222);
 		si.autentica(fa);
 		
