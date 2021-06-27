c181694c5d84c532db536d83ebcf76c353d5cca1
Paulo Sérgio <76066880+FtxDante@users.noreply.github.com>
2021-06-27 17:03:58 -0300

    Logs java

--- exercicios/src/exercicios/exercicio1.java
+++ exercicios/src/exercicios/exercicio1.java
@@ -2,6 +2,18 @@ package exercicios;
 
 import java.util.Scanner;
 
+/** 
+ * <hr>
+ * <article style="background:linear-gradient(45deg,aqua 10%, aquamarine); padding: 1rem; border-radius:10px; border:1px solid black;">
+ * <h2 style="font-weight:bold; font-size:1.5em; margin:0;">Como responder:</h2>
+ * Sua <em style="font-weight:bold;">idade</em> em anos deve ser seu ultimo aniversário <br>
+ * Seus <em style="font-weight:bold;">meses</em> devem ser em que mês você está agora <br>
+ * Seus <em style="font-weight:bold;">dias</em> devem ser o dia de hoje (Espresso em números). <br>
+ * </article>
+ * 
+ *  	@author Dante Rodrigues
+ */
+
 public class exercicio1 {
 	
 	public static void main(String[] args) {
@@ -9,6 +21,7 @@ public class exercicio1 {
 		//mostre-a expressa em dias. Leve em consideração o ano com 365 dias e o mês com 30.
 		//(Ex: 3 anos, 2 meses e 15 dias = 1170 dias.)
 
+		
 		
 		Scanner ler = new Scanner(System.in);
 		
@@ -19,6 +32,7 @@ public class exercicio1 {
 		meses = ler.nextInt();
 		System.out.println("Quantos dias você tem?");
 		dias = ler.nextInt();
+		
 		soma = anos*365 + meses*30 + dias;
 		
 		
